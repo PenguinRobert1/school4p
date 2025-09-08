45386d46bc983b1f9a44621c596e27eecaaf964c
Robert <152218269+PenguinRobert1@users.noreply.github.com>
2025-09-08 16:21:10 +0000

    updated

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+
+
+(function() {
+  const galaxyElement = document.getElementById('galaxy');
+  galaxyElement.style.position = 'absolute';
+  galaxyElement.style.top = '0px';
+  galaxyElement.style.left = '0px';
+})();
+
+    (function() {
+      const mainElement = document.getElementById('main');
+      mainElement.style.position = 'absolute';
+      mainElement.style.top = '7px';
+      mainElement.style.left = '10px';
+    })();
